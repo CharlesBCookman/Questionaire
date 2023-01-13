d16837482b1b9dd7ae60c82eef52f95d0656b265
CharlesBCookman <CharlesBCookman@gmail.com>
2023-01-13 12:58:02 -0800

    set button and div variables in js

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function(event){
+  const startButton = document.getElementById("startbutton");
+  const q1Button = document.getElementById("q1button");
+  const q2Button = document.getElementById("q2button");
+  const q3Button = document.getElementById("q3button");
+  const q4Button = document.getElementById("q4button");
+  const q5Button = document.getElementById("q5button");
+  const reset = document.getElementById("reset");
+  const q1div = document.getElementById("divq1");
+  const q2div = document.getElementById("divq2");
+  const q3div = document.getElementById("divq3");
+  const q4div = document.getElementById("divq4");
+  const q5div = document.getElementById("divq5");
+  const answersdiv = document.getElementById("answersdiv");
+});
