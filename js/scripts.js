aaea447527f051531dcb04e1baff3047fa5ea7f3
CharlesBCookman <CharlesBCookman@gmail.com>
2023-01-13 16:56:57 -0800

    WIP buttons not working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+// function runEvaluation(p, j, c) {
+//   p = document.getElementById("python");
+//   j = document.getElementById("javascript");
+//   c = document.getElementById("c")
+// }
+
 function question1() {
   const nameArea = document.getElementById("namesection");
   const q1 = document.getElementById("q1div");
@@ -7,36 +13,52 @@ function question1() {
 }
 
 function question2() {
-  document.getElementById("q1div").classList.add("hidden");
-  document.getElementById("q2div").removeAttribute("class");
+  const q1 = document.getElementById("q1div");
+  const q2 = document.getElementById("q2div");
+  q1.classList.add("hidden");
+  q2.removeAttribute("class");
+  console.log("q1 button triggering");
 }
 
 function question3() {
-  document.getElementById("q2div").classList.add("hidden");
-  document.getElementById("q3div").removeAttribute("class");
+  const q2 = document.getElementById("q2div");
+  const q3 = document.getElementById("q3div");
+  q2.classList.add("hidden");
+  q3.removeAttribute("class");
+  console.log("q2 button triggering");
 }
 
 function question4() {
-  document.getElementById("q3div").classList.add("hidden");
-  document.getElementById("q4div").removeAttribute("class");
+  const q3 = document.getElementById("q3div");
+  const q4 = document.getElementById("q4div");
+  q3.classList.add("hidden");
+  q4.removeAttribute("class");
+  console.log("q3 button triggering");
 }
 
 function question5() {
-  document.getElementById("q4div").classList.add("hidden");
-  document.getElementById("q5div").removeAttribute("class");
+  const q4 = document.getElementById("q4div");
+  const q5 = document.getElementById("q5div");
+  q4.classList.add("hidden");
+  q5.removeAttribute("class");
+  console.log("q4 button triggering");
 }
 
 function getAnswer() {
-  document.getElementById("q5div").classList.add("hidden");
-  document.getElementById("answersdiv").removeAttribute("class");
+  const q5 = document.getElementById("q4div");
+  const answer = document.getElementById("q5div");
+  console.log("answer button triggering");
+  q5.classList.add("hidden");
+  answer.removeAttribute("class");
 }
 
 function resetPage() {
   document.getElementById("answersdiv").classList.add("hidden");
   document.getElementById("namesection").removeAttribute("class");
+  console.log("answer button triggering");
 }
 
-window.addEventListener("load", function(event) {
+window.addEventListener("load", function() {
   const startButton = document.getElementById("startbutton");
   const q1Button = document.getElementById("q1button");
   const q2Button = document.getElementById("q2button");
@@ -52,6 +74,7 @@ window.addEventListener("load", function(event) {
   const q5div = document.getElementById("divq5");
   const answersdiv = document.getElementById("answersdiv");
   console.log("divs set");
+
   startButton.addEventListener("click", question1);
   q1Button.addEventListener("click", question2);
   q2Button.addEventListener("click", question3);
@@ -59,6 +82,6 @@ window.addEventListener("load", function(event) {
   q4Button.addEventListener("click", question5);
   q5Button.addEventListener("click", getAnswer);
   reset.addEventListener("click", resetPage);
-})
+});
 
 //input[name='((radio's name))']:checked
